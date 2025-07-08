2727e3b1f3b9b0ec5402abf8dff3e6bfa61e022c
YuraThomas <yurathomass@gmail.com>
2025-07-08 12:15:40 +0300

    Update modern_shift_reg.sv

--- chip_des_school/Logic_Task/modern_shift_reg.sv
+++ chip_des_school/Logic_Task/modern_shift_reg.sv
@@ -1,20 +1,23 @@
-module modern_shift_reg 
-#(parameter N = 8) (
-	input [31:0] data_i,
+module  shift_register_with_valid
+#(parameter depth = 8,
+  parameter width = 32) (
+	input [width -1:0] data_i,
 	input vld_i,
 	input clk_i,
 	input rst_i,
 	output vld_o,
-	output [31:0] data_o
+	output [width -1:0] data_o
 );
 
-logic [31:0] data_trig [N];
-logic vld [N];
+localparam N = depth;
+
+logic [width -1:0] data_trig [N];
+logic [N-1 : 0] vld;
 integer i;
 
 always @(posedge clk_i) begin
 	for (i = 1; i < N; i++ ) begin
-		if (vld[i]) data_trig[i] <= data_trig[i-1];
+		if (vld[i-1]) data_trig[i] <= data_trig[i-1];
 		else data_trig[i] <= data_trig[i];
 	end
 	
@@ -45,4 +48,3 @@ assign data_o = data_trig [N-1];
 assign vld_o = vld [N-1];
 
 endmodule
-	
